87689d298794762b83b7d637ce0dc2842b397ff8
nimrodouko <oukonimrod@gmail.com>
2026-05-28 21:04:00 +0300

    model load

--- renderer.cpp
+++ renderer.cpp
@@ -4,6 +4,15 @@
 
 
 
+Mesh::Mesh(std::vector<Vertex>vertices, std::vector<unsigned int> indices, std::vector<Texture>textures) {
+	this->vertices = vertices;
+	this->indices = indices;
+	this->textures = textures;
+
+	setupmesh();
+}
+
+
  void Mesh::setupmesh() {
 	 glGenVertexArrays(1, &VAO);
 	 glGenBuffers(1, &VBO);
@@ -15,15 +24,44 @@
 	 glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	 glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 
+	
+	 glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE, sizeof(Vertex),(void *)0);
+	 glEnableVertexAttribArray(0);
 
+	 
+	 glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+	 glEnableVertexAttribArray(1);
+	
+	 glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Texturecoods));
+	 glEnableVertexAttribArray(2);
+
+	 glBindVertexArray(0);
 
 }
 
+ void Mesh::Draw(Shader &shader) {
 
-Mesh::Mesh(std::vector<Vertex>vertices, std::vector<unsigned int> indices, std::vector<Texture>textures) {
-	this->vertices = vertices;
-	this->indices = indices;
-	this->textures = textures;
+	 unsigned int diffusenr{ 1 };
+	 unsigned int specularnr{ 1 };
+	 for (unsigned int i{ 0 }; i < textures.size(); i++) {
+		 glActiveTexture(GL_TEXTURE0 + i);
+		 std::string number;
+		 std::string name{ textures[i].type };
+		 if (name == "texture_diffuse")
+			 number = std::to_string(diffusenr++);
+		 else if (name == "texture_specular")
+			 number = std::to_string(specularnr++);
+		 shader.setint(("material." + name + number).c_str(), i);
+		 glBindTexture(GL_TEXTURE_2D, textures[i].id);
 
-	setupmesh();
-}
+	 }
+	 glActiveTexture(GL_TEXTURE0);
+	 glBindVertexArray(VAO);
+	 glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+	 glBindVertexArray(0);
+
+
+ }
+
+
+
--- renderer.h
+++ renderer.h
@@ -1,1 +1,35 @@
 #pragma once
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <string>
+#include "shader.h"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+struct Vertex {
+    glm::vec3 position;
+    glm::vec3 Normal;
+    glm::vec3 Texturecoods;
+
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+};
+
+class Mesh {
+public:
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+
+    Mesh(std::vector<Vertex>vertices, std::vector<unsigned int> indices, std::vector<Texture>textures);
+    void Draw(Shader& shader);
+private:
+    unsigned int VAO, VBO, EBO;
+    void setupmesh();
+};
+
